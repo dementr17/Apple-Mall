5225e058ac96b626e814a89364bbe88883b889cd
Dmitry <dementr17@yandex.ru>
2022-01-05 13:31:26 +0300

    project outline

---
+++ Apple Mall/Apple Mall/View Controllers/FilterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FilterViewController.swift
+//  Apple Mall
+//
+//  Created by Дмитрий Чепанов on 05.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+}
---
+++ Apple Mall/Apple Mall/View Controllers/InfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.swift
+//  Apple Mall
+//
+//  Created by Дмитрий Чепанов on 05.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+}
